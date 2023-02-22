0fccc605cd1a2dad1818653af9061d1f93b5457b
cedricmolinari <c-molinari@efluid.fr>
2023-02-22 15:20:20 +0100

    pb require

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //connexion à la BDD MySQL
-var mysql      = require('mysql2');
+//var mysql      = require('mysql2');
+import mysql from 'mysql2';
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -20,7 +21,8 @@ connection.connect();
 // connection.end();
 
 //création du serveur localhost
-const express = require('express')
+//const express = require('express')
+import express from 'express';
 const app = express()
 const port = 3000
 app.use(express.json())
