57f26149eaf61785290e49a1e6ff78f354796007
cedricmolinari <c-molinari@efluid.fr>
2023-02-25 23:31:07 +0100

    cmol

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 //connexion à la BDD pgsql
-import { Client } from 'pg'
+import pkg from 'pg';
+const { Client } = pkg;
 const pgsql = new Client({
   user: 'doadmin',
   host: 'app-27a8f32e-6c33-4e20-a7b5-f5b159af7b48-do-user-13582571-0.b.db.ondigitalocean.com',
   database: 'AssurePlus',
   password: 'AVNS_aTgfOfY41WZsV5L5Ktu',
   port: '25060'
-})
+});
 pgsql.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
