57d54297b0c9e62b53a20597475b5f6265f24761
cedricmolinari <c-molinari@efluid.fr>
2023-02-28 23:21:13 +0100

    cmol

--- index.js
+++ index.js
@@ -34,9 +34,6 @@ const app = express()
 const port = 8080
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send(index.html);
-});
 
 app.get("/messages", (req, res) => {
   res.send("Hello");
