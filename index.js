ee28e41950d9fee2fe85daf2226ce02d3b562a21
cedricmolinari <c-molinari@efluid.fr>
2023-02-28 22:22:13 +0100

    cmol

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ const app = express()
 const port = 8080
 app.use(express.json())
 
-app.get("/messages", (req, res) => {
+app.get("https://goldfish-app-2-2cozk.ondigitalocean.app/messages", (req, res) => {
   res.send("Hello");
 });
 
-app.get("/:universalURL", (req, res) => {
+app.get("https://goldfish-app-2-2cozk.ondigitalocean.app/:universalURL", (req, res) => {
   res.send("404 URL NOT FOUND");
 });
 
