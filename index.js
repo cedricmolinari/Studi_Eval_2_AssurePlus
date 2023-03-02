d12d79db63f18755ec366510f4283642a8609c8e
cedricmolinari <c-molinari@efluid.fr>
2023-03-02 19:36:12 +0100

    cmol

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ pgsql.connect(function(err) {
 
 //création du serveur localhost
 import express from 'express'
+import path from 'path'
 const app = express()
 const port = 8080
 app.use(express.json())
