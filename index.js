88130524993dbf973945b8f51df521352dafa092
cedricmolinari <c-molinari@efluid.fr>
2023-03-02 21:29:18 +0100

    cmol

--- index.js
+++ index.js
@@ -31,13 +31,22 @@ pgsql.connect(function(err) {
 //création du serveur localhost
 import express from 'express'
 import path from 'path'
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express()
 const port = 8080
 app.use(express.json())
 
+
+
 app.get('/', function(req, res) {
   //res.send('hello world');
-  res.sendFile(path.join('/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get("/messages", (req, res) => {
