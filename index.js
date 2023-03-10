5bfef0d1d2c0c51f54d711c4b0cb019031b61ce8
cedricmolinari <c-molinari@efluid.fr>
2023-03-10 21:41:21 +0100

    cmol

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //création du serveur localhost
-import pgsql from './BDD/connexionBDD.js'
+
 import express from 'express'
 import path from 'path'
 import morgan from 'morgan';
@@ -8,16 +8,30 @@ import config from './config.json' assert { type: "json" };
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
 import functions from './functions.js'
 const { success, error } = functions;
-
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express()
 const port = 8080
+
+import fs from 'fs';
+import pkg from 'pg';
+const { Client } = pkg;
+const pgsql = new Client({
+  user: 'doadmin',
+  host: 'app-27a8f32e-6c33-4e20-a7b5-f5b159af7b48-do-user-13582571-0.b.db.ondigitalocean.com',
+  database: 'AssurePlus',
+  password: 'AVNS_aTgfOfY41WZsV5L5Ktu',
+  port: '25060',
+  ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync('root.crt').toString()
+  },
+});
 
 pgsql.connect((err) => {
   if (err) {
