0ef2189b043836d06e908b2450ba3cf6bbc237d7
cedricmolinari <c-molinari@efluid.fr>
2023-03-15 06:42:34 +0100

    cmol

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const pgsql = new Client({
   host: 'app-27a8f32e-6c33-4e20-a7b5-f5b159af7b48-do-user-13582571-0.b.db.ondigitalocean.com',
   database: 'AssurePlus',
   password: 'AVNS_aTgfOfY41WZsV5L5Ktu',
-  port: '25060',
+  //port: '25060',
   ssl: {
     rejectUnauthorized: false,
     ca: fs.readFileSync('root.crt').toString()
